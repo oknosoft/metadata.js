26d134eb97df6393a98d2698ad9611e172b46ff5
unpete <info@oknosoft.ru>
2015-08-08 01:25:34 +0500

    v0.9.194

--- examples/codex/js/main.js
+++ examples/codex/js/main.js
@@ -1,3 +1,6 @@
+// This file was automatically generated from "codex.lmd.json"
+(function(global,main,modules,modules_options,options){var initialized_modules={},global_eval=function(code){return global.Function("return "+code)()},global_document=global.document,local_undefined,register_module=function(moduleName,module){var output={exports:{}};initialized_modules[moduleName]=1;modules[moduleName]=output.exports;if(!module){module=module||global[moduleName]}else if(typeof module==="function"){var module_require=lmd_require;if(modules_options[moduleName]&&modules_options[moduleName].sandbox&&typeof module_require==="function"){module_require=local_undefined}module=module(module_require,output.exports,output)||output.exports}module=module;return modules[moduleName]=module},lmd_require=function(moduleName){var module=modules[moduleName];var replacement=[moduleName,module];if(replacement){moduleName=replacement[0];module=replacement[1]}if(initialized_modules[moduleName]&&module){return module}if(typeof module==="string"&&module.indexOf("(function(")===0){module=global_eval(module)}return register_module(moduleName,module)},output={exports:{}};for(var moduleName in modules){initialized_modules[moduleName]=0}main(lmd_require,output.exports,output)})
+(this,(function (require, exports, module) { /* wrapped by builder */
 /**
  * Основное окно примеров кода и документации
  * <br />&copy; http://www.oknosoft.ru 2009-2015
@@ -100,3 +103,29 @@ $p.iface.before_route = function (event) {
 };
 
 
+
+}),{
+"tree": {"id":0,
+  "item":[
+    {"id":"0100","text":"Демо-приложения","child":"1",
+      "item":[
+        {"id":"0110", "text":"Элементы управления"},
+        {"id":"0120", "text":"Управление небольшой фирмой"},
+        {"id":"0130", "text":"Бухгалтерия предприятия"},
+        {"id":"0140", "text":"Безбумажное производство"}
+      ]},
+    {"id":"0200", "text":"Объектная модель","child":"1",
+      "item":[
+        {"id":"0210", "text":"MetaEngine"}
+      ]}
+  ]
+},
+"md0100": "# Демо-приложения\r\nПодключение к типовым конфигурациям 1С\r\n===\r\nПримеры реализации javascript-клиентов к типовым конфигурациям 1С",
+"md0110": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"md0120": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"md0130": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"md0140": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"md0200": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"md0210": "{\r\n  \"0100\": {\r\n    content: \"\"\r\n  }\r\n}",
+"js0100": "<!---->\r\nfunction msg(){\r\n\t$p.msg.show_msg({\r\n\t\ttitle: \"Справка\",\r\n\t\ttype: \"alert-info\",\r\n\t\ttext: $p.msg.not_implemented\r\n\t});\r\n}"
+},{},{});
