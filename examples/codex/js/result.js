3a66db1e915e8f9672f4922b67352be886021ccd
unpete <info@oknosoft.ru>
2015-08-10 17:57:34 +0500

    v0.9.195

--- examples/codex/js/result.js
+++ examples/codex/js/result.js
@@ -16,7 +16,22 @@ $p.settings = function (prm, modifiers) {
 };
 
 $p.iface.oninit = function() {
+
+	$p.job_prm.offline = true;
+
+	// создаём основное окно
+	$p.iface.layout_1c();
+	$p.iface.docs.hideHeader();
+
+	// говорим, что мы уже авторизованы на "сервере"
+	$p.ajax.authorized = true;
+
+	// инициализируем метаданные
 	new $p.Meta(require('meta'));
+
+	// загружаем данные
+	$p.eve.from_json_to_data_obj(require('data'));
+
 };
 
 }),{
