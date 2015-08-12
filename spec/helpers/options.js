806b9f46a79691ad1481b660c8a259a521927ff7
unpete <info@oknosoft.ru>
2015-08-13 00:45:13 +0500

    v0.9.195

---
+++ spec/helpers/options.js
@@ -0,0 +1,7 @@
+/**
+ * <br />&copy; http://www.oknosoft.ru 2009-2015
+ * Created 13.08.2015
+ * @module  options
+ */
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
--- spec/tests/init_spec.js
+++ spec/tests/init_spec.js
@@ -41,10 +41,18 @@ describe("События:", function () {
 				wtest.onload = function(){
 
 					$p = wtest.$p;
+
 					$p.iface.oninit = function () {
 						console.log(Date.now());
 					};
+
+					$p.settings = function (prm, modifiers) {
+						console.log(Date.now());
+					};
+
 					spyOn($p.iface, 'oninit').and.callThrough();
+
+					spyOn($p, 'settings').and.callThrough();
 
 					setTimeout(function() {
 						done();
@@ -56,8 +64,9 @@ describe("События:", function () {
 		};
 	});
 
-	it("oninit должен произойти в пределах секунды после загрузки страницы", function() {
+	it("oninit() и settings() должен быть вызваны в пределах секунды после загрузки страницы", function() {
 		expect($p.iface.oninit).toHaveBeenCalled();
+		expect($p.settings).toHaveBeenCalled();
 	});
 
 });
@@ -82,6 +91,7 @@ describe("Диалог авторизации:", function () {
 
 						prm.russian_names = true;
 						prm.rest_path = "/a/unf/odata/standard.odata/";
+						prm.rest = true;
 						prm.data_url = "examples/unf/data/";
 						prm.create_tables = "examples/unf/data/create_tables.sql";
 
@@ -89,22 +99,23 @@ describe("Диалог авторизации:", function () {
 
 					$p.iface.oninit = function () {
 
-						$p.iface.layout_1c();
-
-						$p.iface.frm_auth(
-							null,
-							function () {
-								//$p.iface.set_hash("doc.СчетНаОплатуПокупателю", "", "", "oper");
-								$p.iface.docs.hideHeader();
-
-							},
-							function (err) {
-								console.log(err);
-							}
-						);
-
 						if(!was_done){
 							was_done = true;
+
+							$p.iface.layout_1c();
+
+							$p.iface.frm_auth(
+								null,
+								function () {
+									//$p.iface.set_hash("doc.СчетНаОплатуПокупателю", "", "", "oper");
+									$p.iface.docs.hideHeader();
+
+								},
+								function (err) {
+									console.log(err);
+								}
+							);
+
 							done();
 						}
 					};
@@ -131,21 +142,78 @@ describe("Диалог авторизации:", function () {
 
 describe("Aвторизация:", function () {
 
-	var wtest,	$p;
+	var wtest, $p, was_done;
 
 	beforeEach(function() {
-
 		wtest = frames[0];
 		$p = wtest.$p;
+	});
+
+	it("должна быть ошибка при неверном логине-пароле", function(done) {
+
+		$p.iface.auth.setItemValue("type", "auth");
+		$p.iface.auth.setItemValue("login", "Плохой логин");
+		$p.iface.auth.btn_click("submit");
+
+		setTimeout(function () {
+
+			// признак авторизованности в $p.ajax
+			expect($p.ajax.authorized).toBeFalsy();
+
+			// диалог с ошибкой
+			var error = wtest.document.querySelector(".dhtmlx-alert-error"), btn;
+			expect(error).toBeDefined();
+			if(error){
+				btn = error.querySelector(".dhtmlx_popup_button");
+				btn.click();
+			}
+
+			done();
+
+		}, 2000);
 
 	});
 
-	it("должна быть ошибка при неверном логине-пароле", function() {
-		expect(typeof $p).toBe("object");
+	it("должен случиться callback при успешной авторизации", function(done) {
+
+		$p.iface.auth.unload();
+
+		$p.iface.frm_auth(
+			null,
+			on_success,
+			function (err) {
+				console.log(err);
+			}
+		);
+
+		function on_success(){
+
+			if(!was_done){
+
+				was_done = true;
+
+				// признак авторизованности в $p.ajax
+				expect($p.ajax.authorized).toBeTruthy();
+
+				// диалог с ошибкой должен отсутствовать
+				expect(wtest.document.querySelector(".dhtmlx-alert-error")).toBeNull();
+
+				$p.iface.docs.hideHeader();
+
+				done();
+			}
+		}
+
+		$p.iface.auth.setItemValue("type", "auth");
+		$p.iface.auth.setItemValue("login", "Дилер");
+		$p.iface.auth.setItemValue("password", "");
+		$p.iface.auth.btn_click("submit");
+
+		setTimeout(on_success, 6000);
 	});
 
-	it("должен случиться callback при успешной авторизации", function() {
-		expect(typeof $p).toBe("object");
+	it("Справочник номенклатуры должен существовать и содержать элементы", function() {
+		expect($p.cat.Номенклатура.alatable.length).toBeGreaterThan(50);
 	});
 
 });
