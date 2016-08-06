955e6abf8a9005843c007e8edd97786c1ae98120
unpete <info@oknosoft.ru>
2016-08-07 00:21:35 +0500

    v0.11.216

--- dist/metadata.js
+++ dist/metadata.js
@@ -1,5 +1,5 @@
 /*!
- metadata.js v0.11.216, built:2016-08-05 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
+ metadata.js v0.11.216, built:2016-08-06 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
  metadata.js may be freely distributed under the AGPL-3.0. To obtain _Oknosoft Commercial license_, contact info@oknosoft.ru
  */
 (function(root, factory) {
@@ -2169,6 +2169,7 @@ function WSQL(){
 
 /**
  * Описание структуры колонки формы списка
+ * @class Col_struct
  * @param id
  * @param width
  * @param type
@@ -2439,7 +2440,315 @@ function InterfaceObjs(){
 		return false
 	};
 
+	/**
+	 * Конструктор описания колонки динамического списка
+	 * @type {Col_struct}
+	 * @constructor
+	 */
 	this.Col_struct = Col_struct;
+
+	/**
+	 *
+	 * @param items {Array} - закладки сайдбара
+	 * @param buttons {Array} - кнопки дополнительной навигации
+	 * @param [icons_path] {String} - путь к иконам сайдбара
+	 */
+	this.init_sidebar = function (items, buttons, icons_path) {
+
+		// наблюдатель за событиями авторизации и синхронизации
+		iface.btn_auth_sync = new iface.OBtnAuthSync();
+
+		// кнопки навигации справа сверху
+		iface.btns_nav = function (wrapper) {
+			return iface.btn_auth_sync.bind(new iface.OTooolBar({
+				wrapper: wrapper,
+				class_name: 'md_otbnav',
+				width: '260px', height: '28px', top: '3px', right: '3px', name: 'right',
+				buttons: buttons,
+				onclick: function (name) {
+					iface.main.cells(name).setActive(true);
+					return false;
+				}
+			}))
+		};
+
+		// основной сайдбар
+		iface.main = new dhtmlXSideBar({
+			parent: document.body,
+			icons_path: icons_path || "dist/imgs/",
+			width: 180,
+			header: true,
+			template: "tiles",
+			autohide: true,
+			items: items,
+			offsets: {
+				top: 0,
+				right: 0,
+				bottom: 0,
+				left: 0
+			}
+		});
+
+		// подписываемся на событие навигации по сайдбару
+		iface.main.attachEvent("onSelect", function(id){
+
+			var hprm = $p.job_prm.parse_url();
+			if(hprm.view != id)
+				iface.set_hash(hprm.obj, hprm.ref, hprm.frm, id);
+
+			iface["view_" + id](iface.main.cells(id));
+
+		});
+
+		// включаем индикатор загрузки
+		iface.main.progressOn();
+
+		// активируем страницу
+		hprm = $p.job_prm.parse_url();
+		if(!hprm.view || iface.main.getAllItems().indexOf(hprm.view) == -1){
+			iface.set_hash(hprm.obj, hprm.ref, hprm.frm, "doc");
+		} else
+			setTimeout(iface.hash_route);
+	};
+
+	/**
+	 * ### Страница "Все объекты"
+	 * похожа на подменю "все функции" тонкого клиента 1С
+	 *
+	 * @class All_meta_objs
+	 * @param cont {dhtmlXCellObject} - контейнер для размещения страницы
+	 * @param [classes] {Array} - список классов. Если параметр пропущен, будут показаны все классы метаданных
+	 * @param [frm_attr] {Object} - дополнительные настройки, которые будут переданы создаваемым формам списка
+	 * @constructor
+	 */
+	function All_meta_objs(cont, classes, frm_attr) {
+
+		this.layout = cont.attachLayout({
+			pattern: "2U",
+			cells: [{
+				id: "a",
+				text: "Разделы",
+				collapsed_text: "Разделы",
+				width: 220
+			}, {
+				id: "b",
+				text: "Раздел",
+				header: false
+			}],
+			offsets: { top: 0, right: 0, bottom: 0, left: 0}
+		});
+
+		// дерево используемых метаданных
+		this.tree = this.layout.cells("a").attachTreeView();
+		this.tree.attachEvent("onSelect", function (name, mode) {
+			if(!mode)
+				return;
+			var mgr = $p.md.mgr_by_class_name(name);
+			if(mgr instanceof DataProcessorsManager){
+				// для отчетов и обработок используем форму отчета
+				mgr.form_rep(this.layout.cells("b"), frm_attr || {hide_header: true});
+
+			}else if(mgr){
+				// для остальных объектов показываем форму списка
+				mgr.form_list(this.layout.cells("b"), frm_attr || {hide_header: true});
+			}
+
+		}.bind(this));
+
+
+		if(!classes){
+			var md_classes = $p.md.get_classes();
+			classes = [];
+			for(var cl in md_classes){
+				if(md_classes[cl].length)
+					classes.push(cl);
+			}
+		}
+
+		// если тип объектов только один, скрываем иерархию
+		if(classes.length == 1){
+			$p.md.get_classes()[classes[0]].forEach(function (name) {
+				var key = classes[0]+"."+name,
+					meta = $p.md.get(key);
+				if(!meta.hide){
+					this.tree.addItem(key, meta.list_presentation || meta.synonym);
+					this.tree.setItemIcons(key, {file: "icon_1c_"+classes[0]});
+				}
+			}.bind(this));
+
+		}else{
+			classes.forEach(function (id) {
+				this.tree.addItem(id, $p.msg["meta_"+id]);
+				this.tree.setItemIcons(id, {file: "icon_1c_"+id, folder_opened: "icon_1c_"+id, folder_closed: "icon_1c_"+id});
+				$p.md.get_classes()[id].forEach(function (name) {
+					var key = id+"."+name,
+						meta = $p.md.get(key);
+					if(!meta.hide){
+						this.tree.addItem(key, meta.list_presentation || meta.synonym, id);
+						this.tree.setItemIcons(key, {file: "icon_1c_"+id});
+					}
+				}.bind(this));
+			}.bind(this));
+		}
+	}
+
+	/**
+	 * ### Страница "Все объекты"
+	 * @property All_meta_objs
+	 * @type {All_meta_objs}
+	 */
+	this.All_meta_objs = All_meta_objs;
+
+	/**
+	 * ### Страница общих настроек
+	 * @param cont {dhtmlXCellObject} - контейнер для размещения страницы
+	 * @constructor
+	 */
+	function Setting2col(cont) {
+
+		// закладка основных настроек
+		cont.attachHTMLString($p.injected_data['view_settings.html']);
+		this.cont = cont.cell.querySelector(".dhx_cell_cont_tabbar");
+		this.cont.style.overflow = "auto";
+
+		// первая колонка настроек
+		this.form1 = (function (cont) {
+
+			var form = new dhtmlXForm(cont, [
+
+				{ type:"settings", labelWidth:80, position:"label-left"  },
+
+				{type: "label", labelWidth:320, label: "Адрес CouchDB", className: "label_options"},
+				{type:"input" , inputWidth: 220, name:"couch_path", label:"Путь:", validate:"NotEmpty"},
+				{type:"template", label:"",value:"",
+					note: {text: "Можно указать как относительный, так и абсолютный URL публикации CouchDB", width: 320}},
+
+				{type: "label", labelWidth:320, label: "Адрес http сервиса 1С", className: "label_options"},
+				{type:"input" , inputWidth: 220, name:"rest_path", label:"Путь", validate:"NotEmpty"},
+				{type:"template", label:"",value:"",
+					note: {text: "Можно указать как относительный, так и абсолютный URL публикации 1С OData", width: 320}},
+
+				{type: "label", labelWidth:320, label: "Значение разделителя данных", className: "label_options"},
+				{type:"input" , inputWidth: 220, name:"zone", label:"Зона:", numberFormat: ["0", "", ""], validate:"NotEmpty,ValidInteger"},
+				{type:"template", label:"",value:"", note: {text: "Для неразделенной публикации, зона = 0", width: 320}},
+
+				{type: "label", labelWidth:320, label: "Суффикс базы пользователя", className: "label_options"},
+				{type:"input" , inputWidth: 220, name:"couch_suffix", label:"Суффикс:"},
+				{type:"template", label:"",value:"",
+					note: {text: "Назначается абоненту при регистрации", width: 320}},
+
+				{type: "label", labelWidth:320, label: "Сохранять пароль пользователя", className: "label_options"},
+				{type:"checkbox", name:"enable_save_pwd", label:"Разрешить:", checked: $p.wsql.get_user_param("enable_save_pwd", "boolean")},
+				{type:"template", label:"",value:"", note: {text: "Не рекомендуется, если к компьютеру имеют доступ посторонние лица", width: 320}},
+				{type:"template", label:"",value:"", note: {text: "", width: 320}},
+
+				{type:"block", blockOffset: 0, name:"block_buttons", list:[
+					{type: "button", name: "save", value: "<i class='fa fa-floppy-o fa-lg'></i>", tooltip: "Применить настройки и перезагрузить программу"},
+					{type:"newcolumn"},
+					{type: "button", offsetLeft: 20, name: "reset", value: "<i class='fa fa-refresh fa-lg'></i>", tooltip: "Стереть справочники и перезаполнить данными сервера"}
+				]
+				}
+			]);
+
+			form.cont.style.fontSize = "100%";
+
+			// инициализация свойств
+			["zone", "couch_path", "couch_suffix", "rest_path"].forEach(function (prm) {
+				if(prm == "zone")
+					form.setItemValue(prm, $p.wsql.get_user_param(prm));
+				else
+					form.setItemValue(prm, $p.wsql.get_user_param(prm) || $p.job_prm[prm]);
+			});
+
+			form.attachEvent("onChange", function (name, value, state){
+				$p.wsql.set_user_param(name, name == "enable_save_pwd" ? state || "" : value);
+			});
+
+			form.disableItem("couch_suffix");
+
+			if(!$p.job_prm.rest_path)
+				form.disableItem("rest_path");
+
+			form.attachEvent("onButtonClick", function(name){
+
+				if(name == "save"){
+
+					// завершаем синхронизацию
+					$p.wsql.pouch.log_out();
+
+					// перезагружаем страницу
+					setTimeout(function () {
+						$p.eve.redirect = true;
+						location.reload(true);
+					}, 1000);
+
+				} else if(name == "reset"){
+
+					dhtmlx.confirm({
+						title: "Сброс данных",
+						text: "Стереть справочники и перезаполнить данными сервера?",
+						cancel: $p.msg.cancel,
+						callback: function(btn) {
+							if(btn)
+								$p.wsql.pouch.reset_local_data();
+						}
+					});
+				}
+			});
+
+			return form;
+
+		})(this.cont.querySelector("[name=form1]").firstChild);
+
+		// вторая колонка настроек
+		this.form2 = (function (cont) {
+
+			var form = new dhtmlXForm(cont, [
+				{ type:"settings", labelWidth:320, position:"label-left"  },
+
+				{type: "label", label: "Тип устройства", className: "label_options"},
+				{ type:"block", blockOffset: 0, name:"block_device_type", list:[
+					{ type:"settings", labelAlign:"left", position:"label-right"  },
+					{ type:"radio" , name:"device_type", labelWidth:120, label:'<i class="fa fa-desktop"></i> Компьютер', value:"desktop"},
+					{ type:"newcolumn"   },
+					{ type:"radio" , name:"device_type", labelWidth:150, label:'<i class="fa fa-mobile fa-lg"></i> Телефон, планшет', value:"phone"}
+				]  },
+				{type:"template", label:"",value:"", note: {text: "Класс устройства определяется автоматически, но пользователь может задать его явно", width: 320}},
+
+				{type: "label", label: "Подключаемые модули", className: "label_options"},
+				{type:"input" , position:"label-top", inputWidth: 320, name:"modifiers", label:"Модификаторы:", value: $p.wsql.get_user_param("modifiers"), rows: 3, style:"height:80px;"},
+				{type:"template", label:"",value:"", note: {text: "Список дополнительных модулей", width: 320}}
+
+			]);
+
+			form.cont.style.fontSize = "100%";
+
+			// инициализация свойств
+			form.checkItem("device_type", $p.job_prm.device_type);
+
+			// подключаем обработчик изменения значений в форме
+			form.attachEvent("onChange", function (name, value, state){
+				$p.wsql.set_user_param(name, value);
+
+			});
+
+			form.disableItem("modifiers");
+			form.getInput("modifiers").onchange = function () {
+				$p.wsql.set_user_param("modifiers", this.value);
+			};
+
+			return form;
+
+		})(this.cont.querySelector("[name=form2]").firstChild);
+
+	}
+
+	/**
+	 * ### Страница общих настроек
+	 * @property Setting2col
+	 * @type {Setting2col}
+	 */
+	this.Setting2col = Setting2col;
 }
 
 $p.__define({
@@ -2467,7 +2776,7 @@ $p.__define({
 		get: function () {
 			return $p.cat && $p.cat.users ?
 				$p.cat.users.by_id($p.wsql.get_user_param("user_name")) :
-				$p.cat.users.get();
+				$p.utils.blank.guid;
 		}
 	},
 
@@ -2480,7 +2789,7 @@ $p.__define({
 	 */
 	current_acl: {
 		get: function () {
-			var res;
+			var res = {};
 			if($p.cat && $p.cat.users_acl){
 				$p.cat.users_acl.find_rows({owner: $p.current_user}, function (o) {
 					res = o;
@@ -15001,7 +15310,9 @@ DataManager.prototype.form_selection = function(pwnd, attr){
 						grid.reload();
 				}, 20);
 			});
-			tree.attachEvent("onSelect", function(){	// довешиваем обработчик на дерево
+			tree.attachEvent("onSelect", function(id, mode){	// довешиваем обработчик на дерево
+				if(!mode)
+					return;
 				if(this.do_not_reload)
 					delete this.do_not_reload;
 				else
