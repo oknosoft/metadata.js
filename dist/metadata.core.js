5553f30b7b7836b7a52b5c68af39b5c9837cb39f
unpete <info@oknosoft.ru>
2017-02-25 03:05:56 +0500

    компактные кнопки в OTabular

--- dist/metadata.core.js
+++ dist/metadata.core.js
@@ -1,5 +1,5 @@
 /*!
- metadata.js v0.12.226, built:2017-02-16 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
+ metadata.js v0.12.226, built:2017-02-24 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
  metadata.js may be freely distributed under the AGPL-3.0. To obtain _Oknosoft Commercial license_, contact info@oknosoft.ru
  */
 (function(root, factory) {
