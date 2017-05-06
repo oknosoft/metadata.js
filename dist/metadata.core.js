5b2e7d4d41682213637308ac48f738ac7a7bb907
unpete <info@oknosoft.ru>
2017-05-06 22:50:10 +0500

    update package.json

--- dist/metadata.core.js
+++ dist/metadata.core.js
@@ -1,5 +1,5 @@
 /*!
- metadata.js v0.12.226, built:2017-05-05 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
+ metadata.js v0.12.226, built:2017-05-06 &copy; Evgeniy Malyarov http://www.oknosoft.ru 2014-2016
  metadata.js may be freely distributed under the AGPL-3.0. To obtain _Oknosoft Commercial license_, contact info@oknosoft.ru
  */
 (function(root, factory) {
