5765f0f8f067f4a05167022cc6241efc01e434da
unpete <info@oknosoft.ru>
2018-01-08 22:26:13 +0500

    откат к pouchdb.authentication v0.5.5 в metadata-transition

--- packages/metadata-superlogin/index.js
+++ packages/metadata-superlogin/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-superlogin v2.0.16-beta.45, built:2018-01-01
+ metadata-superlogin v2.0.16-beta.45, built:2018-01-08
  © 2014-2017 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
