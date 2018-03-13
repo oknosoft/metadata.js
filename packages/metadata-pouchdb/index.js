d49ef34faf462b3530d7c253946441fb82d1f742
unpete <info@oknosoft.ru>
2018-03-13 23:37:54 +0500

    pre-2.0.16-beta.54

--- packages/metadata-pouchdb/index.js
+++ packages/metadata-pouchdb/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-pouchdb v2.0.16-beta.53, built:2018-03-13
+ metadata-pouchdb v2.0.16-beta.54, built:2018-03-13
  © 2014-2018 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
