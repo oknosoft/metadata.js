edda552fec34d5e9f9a9ff44825746afc6e08748
unpete <info@oknosoft.ru>
2018-04-05 14:57:47 +0500

    отладка формы отчета

--- packages/metadata-react/FrmReport/RepTabularSection.js
+++ packages/metadata-react/FrmReport/RepTabularSection.js
@@ -65,7 +65,7 @@ export default class RepTabularSection extends Component {
     const {_obj, _tabular, scheme} = this.props;
     const dims = scheme.dims();
     const rows = _obj[_tabular]._rows || [];
-    if(rows.length && dims.length && !dims[0]){
+    if(rows.length && dims.length /*&& !dims[0]*/){
       const srows = rows.slice(0);
       const subRows = rows[0].children;
       srows.splice(1, 0, ...subRows);
