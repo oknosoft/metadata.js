bd21a2996599f0f4607910ff169a90c49fc0012d
unpete <info@oknosoft.ru>
2018-04-05 15:58:29 +0500

    отладка формы отчета

--- packages/metadata-react/FrmReport/RepTabularSection.js
+++ packages/metadata-react/FrmReport/RepTabularSection.js
@@ -61,6 +61,23 @@ export default class RepTabularSection extends Component {
     };
   }
 
+  shouldComponentUpdate({scheme}) {
+    if(scheme !== this.props.scheme) {
+      // в случае непустого результата - чистим
+      const {_obj, _tabular} = this.props;
+      const tabular = _obj[_tabular];
+      if(tabular && tabular.count()){
+        if(tabular._rows){
+          tabular._rows.length = tabular._rows._count = 0;
+        }
+        tabular.clear();
+      }
+      this.state.rows.length = 0;
+      this.state.expanded = {};
+    }
+    return true;
+  }
+
   expandRoot() {
     const {_obj, _tabular, scheme} = this.props;
     const dims = scheme.dims();
