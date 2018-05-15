c92b13ee9612056374ac3dd6ac8c490036ef2aa7
unpete <info@oknosoft.ru>
2018-05-15 10:58:35 +0500

    поисковые боты в browser_compatible()

--- packages/metadata-react/App/BrowserCompatibility.js
+++ packages/metadata-react/App/BrowserCompatibility.js
@@ -18,7 +18,8 @@ export function browser_compatible(ie11) {
   // navigator.userAgent.match(/(Chrome|Opera|YaBrowser)/) && !navigator.userAgent.match(/Edge/);
 
   return (typeof Promise == 'function' && typeof Proxy == 'function' && typeof Symbol == 'function') ||
-    (ie11 && navigator.userAgent.match(/Trident\/7.0/) && navigator.userAgent.match(/11.0/));
+    (ie11 && navigator.userAgent.match(/Trident\/7.0/) && navigator.userAgent.match(/11.0/)) ||
+    navigator.userAgent.match(/Google|Yandex|Slurp|ia_archiver/i);
 }
 
 function BrowserCompatibility({classes}) {
