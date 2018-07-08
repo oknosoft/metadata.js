cf1be4b38e992bcdd5be8343128249585bf04143
unpete <info@oknosoft.ru>
2018-07-08 19:16:59 +0500

    аккуратное вычисление базы в log_manager

--- packages/metadata-abstract-ui/src/log_manager.js
+++ packages/metadata-abstract-ui/src/log_manager.js
@@ -97,8 +97,8 @@ export default function log_manager() {
       if(dfrom === 'stamp' && pouch.authorized) {
         dfrom = this._stamp;
         if(!pouch.remote.log) {
-          const {__opts} = pouch.remote.ram;
-          pouch.remote.log = new classes.PouchDB(__opts.name.replace(/ram$/, 'log'),
+          const {__opts} = (pouch.remote.ram || pouch.remote.remote || pouch.remote.doc);
+          pouch.remote.log = new classes.PouchDB(__opts.name.replace(/(ram|remote|doc)$/, 'log'),
             {skip_setup: true, adapter: 'http', auth: __opts.auth});
         }
 
