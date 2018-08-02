a943fdd280a07aae2375a85e8db06eb6601ea3b0
unpete <info@oknosoft.ru>
2018-08-02 22:26:41 +0500

    рефакторинг reporting (es6)

--- packages/metadata-core/index.js
+++ packages/metadata-core/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-core v2.0.17-beta.5, built:2018-07-31
+ metadata-core v2.0.17-beta.5, built:2018-08-02
  © 2014-2018 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
