8bd85aeb6a9155cc87d48c82632f3c898082a716
unpete <info@oknosoft.ru>
2018-08-08 09:14:40 +0500

    2.0.17-beta.6

--- packages/metadata-core/index.js
+++ packages/metadata-core/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-core v2.0.17-beta.5, built:2018-08-02
+ metadata-core v2.0.17-beta.6, built:2018-08-08
  © 2014-2018 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
@@ -4416,7 +4416,7 @@ class MetaEngine {
     this.md.off(type, listener);
   }
   get version() {
-    return '2.0.17-beta.5';
+    return '2.0.17-beta.6';
   }
   toString() {
     return 'Oknosoft data engine. v:' + this.version;
