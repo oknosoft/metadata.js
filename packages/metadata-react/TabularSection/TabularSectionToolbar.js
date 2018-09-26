93fbefecf5fa4ce1390211341d383c7760d2aa33
unpete <info@oknosoft.ru>
2018-09-26 19:39:41 +0500

    (#378) унификация формы объекта и табчасти

--- packages/metadata-react/TabularSection/TabularSectionToolbar.js
+++ packages/metadata-react/TabularSection/TabularSectionToolbar.js
@@ -64,7 +64,7 @@ class TabularSectionToolbar extends Component {
         {[
           !denyAddDel && <IconButton key="btn_add" title="Добавить строку" onClick={props.handleAdd}><AddIcon /></IconButton>,
           !denyAddDel && <IconButton key="btn_del" title="Удалить строку" onClick={props.handleRemove}><RemoveIcon /></IconButton>,
-          !denyAddDel && <IconButton key="btn_del" title="Удалить все строки" onClick={props.handleClear}><RemoveAllIcon /></IconButton>,
+          !denyAddDel && <IconButton key="btn_clear" title="Удалить все строки" onClick={props.handleClear}><RemoveAllIcon /></IconButton>,
           !denyAddDel && !denyReorder && <IconButton key="sep1" disabled>|</IconButton>,
           !denyReorder && <IconButton key="btn_up" title="Переместить вверх" onClick={handleUp}><ArrowUpIcon/></IconButton>,
           !denyReorder && <IconButton key="btn_down"  title="Переместить вниз" onClick={handleDown}><ArrowDownIcon/></IconButton>,
