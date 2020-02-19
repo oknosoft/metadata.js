a7a791f3f3d4d4c6d44b02ad815a3b55d93110a1
unpete <info@oknosoft.ru>
2020-02-19 12:09:14 +0500

    2.0.22-beta.3

--- packages/metadata-superlogin/index.js
+++ packages/metadata-superlogin/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-superlogin v2.0.22-beta.2, built:2020-01-26
+ metadata-superlogin v2.0.22-beta.3, built:2020-02-19
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
