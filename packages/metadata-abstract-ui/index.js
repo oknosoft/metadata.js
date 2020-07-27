34712db81ffd57d7d2faf64a3c11489353867a96
unpete <info@oknosoft.ru>
2020-07-27 10:39:27 +0500

    2.0.23-beta.3

--- packages/metadata-abstract-ui/index.js
+++ packages/metadata-abstract-ui/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-abstract-ui v2.0.23-beta.2, built:2020-07-22
+ metadata-abstract-ui v2.0.23-beta.3, built:2020-07-27
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
