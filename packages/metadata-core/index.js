ec3aa91a8de7e7ac7e1708d4e87cf43bd46b0471
unpete <info@oknosoft.ru>
2020-11-27 01:32:12 +0500

    redirect 401 для всех запросов к auth-proxy без авторизации

--- packages/metadata-core/index.js
+++ packages/metadata-core/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-core v2.0.23-beta.6, built:2020-11-25
+ metadata-core v2.0.23-beta.7, built:2020-11-26
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
@@ -4808,7 +4808,7 @@ class MetaEngine {
     this.md.off(type, listener);
   }
   get version() {
-    return "2.0.23-beta.6";
+    return "2.0.23-beta.7";
   }
   toString() {
     return 'Oknosoft data engine. v:' + this.version;
