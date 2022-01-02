e67bd9259154e385ba1c4310e1b6c99e8d7d3c73
unpete <info@oknosoft.ru>
2022-01-02 19:42:12 +0500

    стили в InputReadOnly

--- packages/metadata-react/src/DataField/FieldInfinit/InputReadOnly.js
+++ packages/metadata-react/src/DataField/FieldInfinit/InputReadOnly.js
@@ -2,29 +2,39 @@ import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField';
 import cn from 'classnames';
 
 export default function InputReadOnly(props) {
-  const {_meta, classes} = props;
-  return props.isTabular ?
-    <div>
+  const {_meta, isTabular, classes, extClasses, className, fullWidth, inputValue, ...other} = props;
+  if(isTabular) {
+    return <div>
       <input
         type="text"
-        value={props.inputValue}
+        value={inputValue}
         title={_meta.tooltip || _meta.synonym}
         placeholder={_meta.synonym || _meta.tooltip}
         readOnly
       />
-    </div>
-    :
-    <TextField
-      disabled
-      className={cn(classes.root, classes.formControl, props.bar && classes.barInput)}
-      fullWidth={props.fullWidth}
-      label={props.label_position === 'hide' ? '' : _meta.synonym}
-      title={_meta.tooltip || _meta.synonym}
-      value={props.inputValue}
-      classes={{input: classes.input}}
-    />;
+    </div>;
+  }
+
+  if(other.fullWidth === undefined) {
+    other.fullWidth = true;
+  }
+  return <FormControl
+      className={extClasses && extClasses.control ? '' : cn(classes.formControl, className, props.bar && classes.barInput)}
+      classes={extClasses && extClasses.control ? extClasses.control : null}
+      onDoubleClick={null}
+      {...other}
+    >
+      {props.label_position != 'hide' && <InputLabel classes={extClasses && extClasses.label ? extClasses.label : null}>
+        {_meta.synonym}
+      </InputLabel>}
+      <Input
+        classes={Object.assign({input: classes.input}, extClasses && extClasses.input)}
+        value={inputValue}
+        readOnly
+      />
+    </FormControl>;
+
 }
