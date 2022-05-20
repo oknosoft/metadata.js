dc61ee4c5ad65ac6dfa27093cda8938229073251
unpete <info@oknosoft.ru>
2022-05-20 12:16:19 +0500

    2.0.30-beta.2

--- packages/metadata-core/index.js
+++ packages/metadata-core/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-core v2.0.30-beta.1, built:2022-05-18
+ metadata-core v2.0.30-beta.2, built:2022-05-20
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
@@ -4983,7 +4983,7 @@ class MetaEngine {
     this.md.off(type, listener);
   }
   get version() {
-    return "2.0.30-beta.1";
+    return "2.0.30-beta.2";
   }
   toString() {
     return 'Oknosoft data engine. v:' + this.version;
--- packages/metadata-superlogin/index.js
+++ packages/metadata-superlogin/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-superlogin v2.0.30-beta.1, built:2022-04-20
+ metadata-superlogin v2.0.30-beta.2, built:2022-05-20
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
