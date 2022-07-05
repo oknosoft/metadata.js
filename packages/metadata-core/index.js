1713721105e76856f645789748100922327941d2
unpete <info@oknosoft.ru>
2022-07-05 12:37:04 +0300

    2.0.30-beta.4

--- packages/metadata-core/index.js
+++ packages/metadata-core/index.js
@@ -1,5 +1,5 @@
 /*!
- metadata-core v2.0.30-beta.3, built:2022-07-05
+ metadata-core v2.0.30-beta.4, built:2022-07-05
  © 2014-2019 Evgeniy Malyarov and the Oknosoft team http://www.oknosoft.ru
  metadata.js may be freely distributed under the MIT
  To obtain commercial license and technical support, contact info@oknosoft.ru
@@ -4989,7 +4989,7 @@ class MetaEngine {
     this.md.off(type, listener);
   }
   get version() {
-    return "2.0.30-beta.3";
+    return "2.0.30-beta.4";
   }
   toString() {
     return 'Oknosoft data engine. v:' + this.version;
