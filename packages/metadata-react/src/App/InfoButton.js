99fd2fec7f38202a59e3c3173d21eb85d52a7540
unpete <info@oknosoft.ru>
2026-03-11 14:56:23 +0500

    onClick в InfoButton

--- packages/metadata-react/src/App/InfoButton.js
+++ packages/metadata-react/src/App/InfoButton.js
@@ -7,8 +7,8 @@ import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 
-export default function InfoButton({text, title = 'Информация'}) {
-  return <IconButton color="primary" onClick={() => $p.ui.dialogs.alert({title, markdown: text})}>
+export default function InfoButton({text, title = 'Информация', onClick}) {
+  return <IconButton color="primary" onClick={onClick || (() => $p.ui.dialogs.alert({title, markdown: text}))}>
     <InfoIcon/>
   </IconButton>;
 };
